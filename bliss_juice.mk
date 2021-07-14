e25c85968e6fa9498093200cbc12fcfa07285c40
darkpewds6969 <taroyjoemar763@gmail.com>
2021-07-14 09:52:41 +0000

    juice: Add Product props for Redfin FP and update from July 2021

--- bliss_juice.mk
+++ bliss_juice.mk
@@ -27,4 +27,14 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210605.005/7349499:user/release-keys"
+# Fingerprint
+
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys"
+BUILD_DESCRIPTION := redfin-user 11 RQ2A.210705.001 7380771 release-keys
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="$(BUILD_DESCRIPTION)"
+
