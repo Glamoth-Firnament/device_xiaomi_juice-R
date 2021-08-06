a70a8bfbc29c1605e3c4f1936a1085e94e4fb9db
dlwlrma123 <alexfinhart@gmail.com>
2021-08-06 05:08:48 +0000

    Let's Crdroid cook!
    Signed-off-by: zaidannn7 <72336864+zaidannn7@users.noreply.github.com>

---
+++ lineage_juice.mk
@@ -0,0 +1,20 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+$(call inherit-product, device/xiaomi/juice/device.mk)
+
+# Inherit some common lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := lineage_juice
+PRODUCT_DEVICE := juice
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := sm6115
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
