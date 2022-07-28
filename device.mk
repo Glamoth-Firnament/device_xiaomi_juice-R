c6c304705a0cbedca8264c8ee3318ea316c0c419
rayrzy <vincentrayananda@gmail.com>
2022-07-28 18:58:22 +0700

    juice: Introduce prebuilt Google Camera Go
    * Google Camera Go Special Build 2 by Greatness
    
    https://www.celsoazevedo.com/files/android/google-camera/dev-greatness/f/dl9/
    
    Changelog:
    - This is an incremental update to my last build
    - Activated Option to Change Aspect Ratio
    - HDR improved Dramatically, Much more natural colors now and Highlights are no more overblown
    - Like last time, I am not responsible for any application other than the one with below hash. So be careful.
    
    Signed-off-by: rayrzy <vincentrayananda@gmail.com>
    Signed-off-by: R.A.P <rhmd.19801@gmail.com>
    Change-Id: I0452f58832b81ee41fc6c3e6359335d6c6d1e0cc

--- device.mk
+++ device.mk
@@ -69,6 +69,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/pixel_2016_exclusive.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/pixel_2016_exclusive.xml
 
+# Google Camera Go
+PRODUCT_PACKAGES += \
+    GoogleCameraGo
+
 # Codec2 modules
 PRODUCT_PACKAGES += \
     com.android.media.swcodec \
