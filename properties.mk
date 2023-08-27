d0060fea849e74e8a40dba2467e3482c65450072
zaidannn7 <72336864+zaidannn7@users.noreply.github.com>
2023-08-27 16:50:00 +0700

    juice: properties: Clean up some redudant properties
    Signed-off-by: zaidannn7 <72336864+zaidannn7@users.noreply.github.com>

--- properties.mk
+++ properties.mk
@@ -70,7 +70,6 @@ PRODUCT_PRODUCT_PROPERTIES += \
     debug.enable.sglscale=1 \
     debug.mdpcomp.logs=0 \
     debug.sf.recomputecrop=0 \
-    ro.surface_flinger.wcg_composition_dataspace=143261696 \
     ro.surface_flinger.running_without_sync_framework=true \
     ro.surface_flinger.use_context_priority=true \
     ro.surface_flinger.has_wide_color_display=false \
@@ -133,33 +132,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.audio.offload.buffer.size.kb=256
     
 
-# Power-saving props
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.use_data_netmgrd=true \
-    ro.vold.umsdirtyratio=20 \
-    ro.ril.disable.power.collapse=0 \
-    power.saving.mode=1 \
-    pm.sleep_mode=1
-
 # Disable Rescue Party
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.sys.disable_rescue=true \
     persist.device_config.configuration.disable_rescue_party=true
-
-#Gboard Configuration by Vern
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.com.google.ime.corner_key_l=35 \
-    ro.com.google.ime.corner_key_r=35 \
-    ro.com.google.ime.themed_nav_bar_style=2 \
-    ro.com.google.ime.silk_theme=true \
-    ro.com.google.ime.pill_shaped_key=true \
-    ro.com.google.ime.enable_keyboard_redesign=true \
-    ro.com.google.ime.use_keyboard_redesign_theme_by_default=true \
-    ro.com.google.ime.kb_pad_port_r=8.0 \
-    ro.com.google.ime.kb_pad_port_l=8.0 \
-    ro.com.google.ime.kb_pad_port_b=1 \
-    ro.com.google.ime.kb_pad_land_r=32.0 \
-    ro.com.google.ime.kb_pad_land_l=32.0
 
 
 # ZRAM
