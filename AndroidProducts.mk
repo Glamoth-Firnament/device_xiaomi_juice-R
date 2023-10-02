61159d0bc9783751033bb99a7a3d16dcfd14ff7a
zaidannn7 <72336864+zaidannn7@users.noreply.github.com>
2023-10-02 20:42:30 +0700

    Carbon moment.
    Signed-off-by: zaidannn7 <72336864+zaidannn7@users.noreply.github.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021 The carbonOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_juice.mk
+    $(LOCAL_DIR)/carbon_juice.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_juice-user \
-    lineage_juice-userdebug \
-    lineage_juice-eng
+    carbon_juice-user \
+    carbon_juice-userdebug \
+    carbon_juice-eng
--- carbon_juice.mk
+++ carbon_juice.mk
@@ -1,17 +1,17 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021 The carbonOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 $(call inherit-product, device/xiaomi/juice/device.mk)
 
-# Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common carbon stuff.
+$(call inherit-product, vendor/carbon/config/common_full_phone.mk)
 
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_juice
+PRODUCT_NAME := carbon_juice
 PRODUCT_DEVICE := juice
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := sm6115
@@ -21,7 +21,5 @@ PRODUCT_MANUFACTURER := Xiaomi
 WITH_GAPPS := false
 WITH_GMS := false
 
-PRODUCT_PRODUCT_PROPERTIES += \
-	ro.cr.maintainer=Scaledzdn. 
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
