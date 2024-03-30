cd8ae4397cafd6a9d1cbc2e476e69f55aa12e8b3
zaidannn7 <72336864+zaidannn7@users.noreply.github.com>
2024-03-30 14:14:55 +0700

    HavocOS-ify
    Signed-off-by: zaidannn7 <72336864+zaidannn7@users.noreply.github.com>

--- havoc_juice.mk
+++ havoc_juice.mk
@@ -11,16 +11,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 $(call inherit-product, device/xiaomi/juice/device.mk)
 
-# Inherit some common AOSP Extended stuff.
-$(call inherit-product, vendor/aosp/common.mk)
+# Inherit some common HavocOS stuff.
+$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_FACE_UNLOCK_SUPPORTED := true
-USE_PIXEL_CHARGER_IMAGES := true
 WITH_GAPPS := false
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_juice
+PRODUCT_NAME := havoc_juice
 PRODUCT_DEVICE := juice
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := sm6115
@@ -28,6 +27,3 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-# Fingerprint
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.211001.001/7641976:user/release-keys
-BUILD_DESCRIPTION := redfin-user 11 RQ3A.211001.001 7641976 release-keys
