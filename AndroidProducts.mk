d86c7c9f6bc968798b932cb36bc2fd5a01b43344
zaidannn7 <72336864+zaidannn7@users.noreply.github.com>
2024-04-06 04:30:09 +0700

    HavocOS-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2021 The carbonOS Project
+# Copyright (C) 2021 The havocOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/carbon_juice.mk
+    $(LOCAL_DIR)/havoc_juice.mk
 
 COMMON_LUNCH_CHOICES := \
-    carbon_juice-user \
-    carbon_juice-userdebug \
-    carbon_juice-eng
+    havoc_juice-user \
+    havoc_juice-userdebug \
+    havoc_juice-eng
--- havoc_juice.mk
+++ havoc_juice.mk
@@ -1,17 +1,17 @@
 #
-# Copyright (C) 2021 The carbonOS Project
+# Copyright (C) 2021 The havocOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 $(call inherit-product, device/xiaomi/juice/device.mk)
 
-# Inherit some common carbon stuff.
-$(call inherit-product, vendor/carbon/config/common.mk)
+# Inherit some common havoc stuff.
+$(call inherit-product, vendor/havoc/config/common.mk)
 
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := carbon_juice
+PRODUCT_NAME := havoc_juice
 PRODUCT_DEVICE := juice
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := sm6115
