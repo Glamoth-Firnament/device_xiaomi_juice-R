d39bc0d1c956f195ac1c9ce83c20d35915e435a7
zaidannn7 <72336864+zaidannn7@users.noreply.github.com>
2024-04-11 15:01:16 +0700

    juice: Use proton clang for build
    Signed-off-by: zaidannn7 <72336864+zaidannn7@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -130,8 +130,8 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
 
 # Kernel Toolchain
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_ADDITIONAL_FLAGS += LLVM=1 \
-	HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
+TARGET_KERNEL_ADDITIONAL_FLAGS += LLVM=1 
+KERNEL_TOOLCHAIN := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-proton/bin
 
 # Metadata
 BOARD_USES_METADATA_PARTITION := true
